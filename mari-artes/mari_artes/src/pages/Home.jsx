2e82a567b5fba4a665e6e98e4e29521845ad3dcb
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-10-17 12:59:36 -0300

    mari-artes - atualizado

--- mari-artes/mari_artes/src/pages/Home.jsx
+++ mari-artes/mari_artes/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 //REACT
+import React from 'react'
 import Footer from '../components/Footer/Footer.jsx' 
 //CSS
 import "./Home.css"
+//VÍDEOS
+import video1mari from "../assets/videos/vídeo-mari-one.mp4"
+import video2mari from "../assets/videos/vídeo-mari-two.mp4"
 //IMAGENS
 import slideMain from "../assets/slide-atual.png"
 import slide2 from "../assets/slide2.png"
@@ -11,6 +15,8 @@ import bracelet_top5 from "../assets/bracelet-top5.png"
 import necklaces_top5 from "../assets/necklaces-top5.png"
 import keychain_top5 from "../assets/keychain-top5.png"
 import photoframe_top5 from "../assets/photoframe-top5.png"
+//ÍCONES
+import { TiArrowRightThick } from "react-icons/ti";
 
 const Home = () => {
 
@@ -28,10 +34,25 @@ const Home = () => {
 
       <div className='frame-mari'>
         <div className='frame-mari-text'>
-          <h3>Vídeo explicativo - título</h3>
-          <p>descrição do vídeo explicativo</p>
+          <h3>Confira aqui!!!</h3>
+          <p>Confira alguns de nossos produtos</p>
         </div>
-          <iframe width="100%" height="315" src="https://www.youtube.com/embed/kchv2fnbMlc?si=nxzlphQy6Svcbw6n" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <div className='videos'>
+            <div className='video-product'>
+              <span><TiArrowRightThick className='icon-video'/> Bolsa de fio náutico preta super estilosa</span>
+              <video controls>
+                <source src={video1mari} type='video/mp4'/>
+                Seu navegador não suporta a execução de vídeos!
+              </video>
+            </div>
+            <div className='video-product'>
+              <span><TiArrowRightThick className='icon-video'/> Bolsa de fio náutico azul jeans muito elegante</span>
+              <video controls>
+                <source src={video2mari} type='video/mp4'/>
+                Seu navegador não suporta a execução de vídeos!
+              </video>
+            </div>
+          </div>
       </div>
 
       <div className='top5-week'>
@@ -39,11 +60,31 @@ const Home = () => {
           <p>Confira aqui o nosso top 5 dos destaques da semana...</p>
 
           <div className='top5-week-products'>
-            <img src={bag_top5} alt="produto1 - top 3" className='img-top5'/>
-            <img src={bracelet_top5} alt="produto2 - top 3" className='img-top5'/>
-            <img src={necklaces_top5} alt="produto3 - top 3" className='img-top5'/>
-            <img src={keychain_top5} alt="produto3 - top 4" className='img-top5'/>
-            <img src={photoframe_top5} alt="produto3 - top 5" className='img-top5'/>
+            <div className='top5-product'>
+              <img src={bag_top5} alt="produto1 - top 5" className='img-top5'/>
+              <span>Bolsa escamada vermelha</span>
+              <span style={{color:"red",textShadow:".3px .3px 1px black",fontSize:"1.3rem"}}>R$ 49,90</span>
+            </div>
+            <div className='top5-product'>
+              <img src={necklaces_top5} alt="produto2 - top 5" className='img-top5'/>
+              <span>Colar de safira azul escuro</span>
+              <span style={{color:"red",textShadow:".3px .3px 1px black",fontSize:"1.3rem"}}>R$ 79,90</span>
+            </div>
+            <div className='top5-product'>
+              <img src={bracelet_top5} alt="produto3 - top 5" className='img-top5'/>
+              <span>Pulseira de cobre dourada</span>
+              <span style={{color:"red",textShadow:".3px .3px 1px black",fontSize:"1.3rem"}}>R$ 20,90</span>
+            </div>
+            <div className='top5-product'>
+              <img src={keychain_top5} alt="produto4 - top 5" className='img-top5'/>
+              <span>Chaveiro pézinho de cachorro</span>
+              <span style={{color:"red",textShadow:".3px .3px 1px black",fontSize:"1.3rem"}}>R$ 10,90</span>
+            </div>
+            <div className='top5-product'>
+              <img src={photoframe_top5} alt="produto5 - top 5" className='img-top5'/>
+              <span>Porta-retrato de madeira </span>
+              <span style={{color:"red",textShadow:".3px .3px 1px black",fontSize:"1.3rem"}}>R$ 13,90</span>
+            </div>
           </div>
       </div>
 
